12b4d11da570c18bc3870359029c89ecc1562a34
Colette J Beckwith <61260684+colettejbeckwith@users.noreply.github.com>
2025-11-25 22:17:43 -0600

    fibonacci sequence working

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,32 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
 
+    let array = [];
+
+    if (typeof x == 'string') {
+        x = parseInt(x);
+    }
+
+    if (x < 0) {
+        return "OOPS";
+    } else if (x == 0) {
+        return 0;
+    } else if (x == 1 || x == 2) {
+        return 1;
+    } else {
+        array[0] = 1;
+        array[1] = 1;
+
+        for (let i = 2; i < x; i++) {
+            array[i] = array[i - 1] + array[i - 2];
+        }
+        return array[x - 1];
+        // console.log(array[x]);
+    }
 };
+
+let result = fibonacci(-10);
+
+console.log(result);
 
 // Do not edit below this line
 module.exports = fibonacci;
