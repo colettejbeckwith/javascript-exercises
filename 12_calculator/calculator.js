cb3102bc64fe0664bcf1a4f454b6d79ea5766e35
Colette J Beckwith <61260684+colettejbeckwith@users.noreply.github.com>
2025-11-25 20:12:01 -0600

    calculator exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,48 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let total = 0;
+  for (let i = 0; i < array.length; i++) {
+    total += array[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let total = 1;
+  for (let i = 0; i < array.length; i++) {
+    total = total * array[i];
+  }
+  return total;
 };
 
-const power = function() {
+const power = function(base, exponent) {
 	
+  let total = 1;
+
+  for (let i = 0; i < exponent; i++) {
+    total = total * base;
+  }
+
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	let i = x;
+  let total = 1;
+  
+  while (i > 0) {
+    total = total * i;
+    i--;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
